72341b58137327c717aaf8476f01552c8e4d39ca
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-28 16:29:21 -0600

    feat: update settings

--- prettier-config.js
+++ prettier-config.js
@@ -1,5 +1,10 @@
 module.exports = {
   singleQuote: true,
   printWidth: 120,
-  endOfLine: 'auto',
+  endOfLine: "auto",
+  semi: false,
+  tabWidth: 2,
+  tabs: false,
+  bracketSpacing: false,
+  trailingComma: "all"
 };
